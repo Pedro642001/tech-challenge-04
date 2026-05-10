9d34d7eb50eb329de9edff68346d6a69ab672c86
pedro-sousa <pedroo642001@gmail.com>
2026-05-10 14:08:11 -0300

    refactor: remove arquivos desnecessários de init.py

--- app/core/routers.py
+++ app/core/routers.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
-from app.controllers import health_check, machine_learning, observability
+from app.controllers.health_check import router as health_router
+from app.controllers.machine_learning import router as machine_learning_router
+from app.controllers.observability import router as observability_router
 from app.core.settings import settings
 
 
 def register_routers(app: FastAPI):
     prefix = settings.API_PREFIX
-    app.include_router(health_check.router, tags=["Health"], prefix=prefix)
-    app.include_router(machine_learning.router, tags=["Machine Learning"], prefix=prefix)
-    app.include_router(observability.router, tags=["Observability"])
+    app.include_router(health_router, tags=["Health"], prefix=prefix)
+    app.include_router(machine_learning_router, tags=["Machine Learning"], prefix=prefix)
+    app.include_router(observability_router, tags=["Observability"])
